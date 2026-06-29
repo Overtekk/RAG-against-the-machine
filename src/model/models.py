3d198935da49152d7dd2e958f8b3f6dfa881929f
Overtekk <theovertekgaming@gmail.com>
2026-06-29 11:38:40 +0200

    add models

---
+++ src/model/models.py
@@ -0,0 +1,111 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  models.py                                         :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: roandrie <roandrie@student.42lehavre.fr   +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/06/29 11:11:55 by roandrie        #+#    #+#               #
+#  Updated: 2026/06/29 11:38:30 by roandrie        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+import uuid
+
+from pydantic import BaseModel, Field
+
+
+class MinimalSource(BaseModel):
+    """
+    Model representing a minimal source of information.
+
+    Attributes:
+        file_path (str): Absolute or relative path to the source file.
+        first_character_index (int): Index of the first character in the source
+        last_character_index (int): Index of the last character in the source.
+    """
+    file_path: str
+    first_character_index: int
+    last_character_index: int
+
+
+class UnansweredQuestion(BaseModel):
+    """
+    Model representing an unanswered question.
+
+    Attributes:
+        question_id (str): uuid of the question.
+        question: raw question.
+    """
+    question_id: str = Field(
+        default_factory=lambda: str(uuid.uuid4())
+    )
+    question: str
+
+
+class AnsweredQuestion(BaseModel):
+    """
+    Model representing an answered question.
+
+    Attributes:
+        sources (list): list of source information.
+        answer: raw question.
+    """
+    sources: list[MinimalSource]
+    answer: str
+
+
+class RagDataset(BaseModel):
+    """
+    Model representing a dataset of RAG questions.
+
+    Attributes:
+        sources (list): list of answered or unanswered questions.
+    """
+    rag_questions: list[AnsweredQuestion | UnansweredQuestion]
+
+
+class MinimalSearchResults(BaseModel):
+    """
+    Model representing the search result.
+
+    Attributes:
+        question_id (str): id of the question.
+        question (str): raw question.
+        retrieved_sources (list): list of source information.
+    """
+    question_id: str
+    question: str
+    retrieved_sources: list[MinimalSource]
+
+
+class MinimalAnswer(BaseModel):
+    """
+    Model representing the search answer.
+
+    Attributes:
+        answer (str): search answer string.
+    """
+    answer: str
+
+
+class StudentSearchResults(BaseModel):
+    """
+    Model representing student search results.
+
+    Attributes:
+        search_results (list): list of search results.
+        k (int): number of results requested.
+    """
+    search_results: list[MinimalSearchResults]
+    k: int
+
+
+class StudentSearchResultsAndAnswer(BaseModel):
+    """
+    Model representing student search results with answers.
+
+    Attributes:
+        search_results (list): list of answers results.
+    """
+    search_results: list[MinimalAnswer]
