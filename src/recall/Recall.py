9114db1fcc2c4cd246dcff009f602c9b50722752
Overtekk <theovertekgaming@gmail.com>
2026-09-03 16:18:26 +0200

    recall3

--- src/recall/Recall.py
+++ src/recall/Recall.py
@@ -6,14 +6,18 @@
 #  By: roandrie <roandrie@student.42lehavre.fr   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/09/01 09:21:52 by roandrie        #+#    #+#               #
-#  Updated: 2026/09/02 13:27:04 by roandrie        ###   ########.fr        #
+#  Updated: 2026/09/03 16:05:38 by roandrie        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
+from enum import Enum
 import json
 from pathlib import Path
 from typing import Any
-from src.model import RagDataset
+
+class Score(Enum):
+    FAILED = 0.0
+    SUCCESS = 1.0
 
 
 class Recall:
@@ -26,7 +30,6 @@ class Recall:
         # Get the data
         with open(self.dataset, 'r', encoding='utf8') as f:
             raw_data_dataset = json.load(f)
-            data_dataset = RagDataset.model_validate(raw_data_dataset)
         with open(file_path, 'r', encoding='utf8') as f:
             raw_data_student = json.load(f)
 
@@ -34,6 +37,26 @@ class Recall:
         student_dict = {
             str(q["question_id"]): q["retrieved_sources"] for q in raw_data_student["search_results"]
         }
+        dataset_dict = {
+            str(item["question_id"]): item["sources"] for item in raw_data_dataset["rag_questions"]
+        }
 
-        for item in data_dataset.rag_questions:
-            print(item.question)
+        score_list: list[float] = []
+        for q_id in dataset_dict:
+            expected_source = dataset_dict.get(q_id, [])
+            if expected_source is None:
+                pass
+
+            retrieved_sources = student_dict.get(q_id, [])
+            if retrieved_sources is None:
+                score_list.append(Score.FAILED)
+
+            # Check if student source is in the expected source
+            for source in expected_source:
+                for chunk in retrieved_sources:
+                    if chunk["file_path"] == source["file_path"]:
+                        if max(chunk["first_character_index"], source["first_character_index"]) < min(chunk["last_character_index"], source["last_character_index"]):
+                            score_list.append(Score.SUCCESS)
+                            break
+                    else:
+                        score_list.append(Score.FAILED)
